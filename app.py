d74668ee1a6b28fff53f6068144e29271f8ab3b8
Hasan-XS <ha3an.majidii@gmail.com>
2024-01-26 23:20:59 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from blueprints.admin import app as admin
 from blueprints.general import app as general
+from blueprints.user import app as user
+from flask_login import LoginManager
 import config
+from model.user import User
 from extention import *
 
 app = Flask(__name__)
+login_manager = LoginManager()
 app.config['SECRET_KEY'] = config.SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
 db.init_app(app)
+login_manager.init_app(app)
 
 app.register_blueprint(admin)
 app.register_blueprint(general)
+app.register_blueprint(user)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 with app.app_context():
     db.create_all()
