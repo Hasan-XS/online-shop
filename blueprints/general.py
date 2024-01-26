741c2c9349db3dd69fb5e86a2b18721defd410c6
Hasan-XS <ha3an.majidii@gmail.com>
2024-01-26 14:57:28 +0330

    Created and display product in home page

---
+++ blueprints/general.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+from model.product import Product
+
+app = Blueprint("general", __name__)
+
+@app.route("/")
+def home():
+    products = Product.query.all()
+    return render_template("general/home.html", products=products)
