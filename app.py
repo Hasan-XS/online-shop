dd25aa7560b76af23a116509b40fe73836e8c31d
Hasan-XS <ha3an.majidii@gmail.com>
2024-01-26 14:55:26 +0330

    Update app.py for register template general

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from blueprints.admin import app as admin
+from blueprints.general import app as general
 import config
 from extention import *
 
@@ -9,6 +10,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
 db.init_app(app)
 
 app.register_blueprint(admin)
+app.register_blueprint(general)
 
 with app.app_context():
     db.create_all()
