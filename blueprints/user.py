0e39dcabdfbe2293ac4fcd912df9e4c445e41839
Hasan-XS <ha3an.majidii@gmail.com>
2024-01-29 20:09:34 +0330

    Update user bluprint for check username and password, and created dashboard route

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
 from passlib.hash import sha256_crypt
 from model.user import User
 from extention import *
@@ -12,16 +12,34 @@ def login():
         return render_template("general/user-login.html")
     else:
         register = request.form.get("regis", None)
-        username = request.form.get("username")
-        password = request.form.get("password")
-        email = request.form.get("email")
+        username = request.form.get("username", None)
+        password = request.form.get("password", None)
+        email = request.form.get("email", None)
 
         if register != None:
+            user = User.query.filter(User.username == username).first()
+            if user != None:
+                flash("Choose another username!")
+                return redirect(url_for("user.login"))
             user = User(username=username, password=sha256_crypt.encrypt(password), email=email)
             db.session.add(user)
             db.session.commit()
             login_user(user)
 
-            return redirect("user.dashboard")
+            return redirect(url_for("user.dashboard"))
         
-        return "false"
+        else:
+            user = User.query.filter(User.username == username).first()
+            if user == None:
+                flash("The username or password is wrong!")
+                return redirect(url_for("user.login"))
+            if sha256_crypt.verify(password, user.password):
+                login_user(user)
+                return redirect(url_for("user.dashboard"))
+            else:
+                flash("The username or password is wrong!")
+                return redirect(url_for("user.login"))
+
+@app.route("/user/dashboard")
+def dashboard():
+    return "dashboard"
