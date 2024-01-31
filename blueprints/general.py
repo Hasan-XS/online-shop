1bcdca0fa25866f43374d6871b6ba9a9f6c63fae
Hasan-XS <ha3an.majidii@gmail.com>
2024-01-31 11:36:02 +0330

    Update general blueprint for show details

--- blueprints/general.py
+++ blueprints/general.py
@@ -8,6 +8,11 @@ def home():
     products = Product.query.all()
     return render_template("general/home.html", products=products)
 
+@app.route("/details/<int:id>/<name>")
+def details(id, name):
+    details = Product.query.filter(Product.id == id).filter(Product.name == name).first_or_404()
+    return render_template("general/details.html", details=details)
+
 @app.route("/about")
 def about():
     return render_template("general/about.html")
