0023243b5924af0fe6b06599c0390fc6efa85bd2
Hasan-XS <ha3an.majidii@gmail.com>
2024-02-11 23:41:02 +0330

    Update payment model

--- model/payment.py
+++ model/payment.py
@@ -1,11 +1,16 @@
 from sqlalchemy import *
 from extention import db, get_current_time
 
+
 class Payment(db.Model):
-    __tablename__ = "Payments"
+    __tablename__ = "payments"
     id = Column(Integer, primary_key=True)
-    status = Column(String, default="pending")
+    status = Column(String, default='pending')
     price = Column(Integer)
-    cart_id = Column(Integer, ForeignKey("Users.id"), nullable=False)
-    cart = db.relationship("Cart", backref="Payments")
-    date_create = Column(String(12), default=get_current_time)
+    token = Column(String)
+    refid = Column(String)
+    transaction_id = Column(String)
+    card_pan = Column(String)
+    date_created = Column(String(15), default=get_current_time)
+    cart_id = Column(Integer, ForeignKey('carts.id'), nullable=False)
+    cart = db.relationship("Cart", backref='payments')
